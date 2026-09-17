dd728d6eaf3ea75d9e10ee47a0add44ba097581e
jagadiswarambati <srijagadishreddy@gmail.com>
2026-09-17 21:37:23 +0530

    Add navigation graph

---
+++ app/src/main/java/com/example/presentation/navigation/NavGraph.kt
@@ -0,0 +1,25 @@
+package com.example.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.presentation.screens.home.HomeScreen
+
+@Composable
+fun PakkaKhataNavGraph(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route,
+        modifier = modifier
+    ) {
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
+    }
+}
